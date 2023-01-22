b629ecf2151d95d9ace1cceb52ac02c9b8e77910
Magnus Fröbom <frobom.magnus@gmail.com>
2023-01-22 14:18:31 +0100

    Plan

--- brain-api/server.js
+++ brain-api/server.js
@@ -5,3 +5,13 @@ const app = express();
 app.listen(3001, () => {
     console.log('App is running on port 3001');
 })
+
+
+/* 
+/ --> res = this is working
+/signin --> POST = succes/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
