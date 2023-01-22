fc5d05deb6ec2054924a9debfe393bd78c93353d
Magnus Fröbom <frobom.magnus@gmail.com>
2023-01-22 19:02:05 +0100

    bcrypt hash added to register.

--- brain-api/server.js
+++ brain-api/server.js
@@ -47,6 +47,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
