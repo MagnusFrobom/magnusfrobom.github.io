958f5ea312e155cf3c9c772728f4c8d91b584f92
Magnus Fröbom <frobom.magnus@gmail.com>
2023-01-24 12:23:41 +0100

    reinstall npm

--- brain-api/server.js
+++ brain-api/server.js
@@ -96,10 +96,6 @@ app.put('/image', (req, res) => {
   .increment('entries', 1)
   .returning('entries')
   .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
     res.json(entries[0].entries);
   })
   .catch(err => res.status(400).json('unable to get entries'))
