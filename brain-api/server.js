e8e083856a035a4cedb42fb37430d8466f23c233
Magnus Fröbom <frobom.magnus@gmail.com>
2023-01-24 22:05:31 +0100

    <Working on server>

--- brain-api/server.js
+++ brain-api/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+// const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register')
 
 const db = knex({
   // Enter your own database information here based on what you created
@@ -10,7 +12,7 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'magnus',
-    password : '',
+    password : 'password',
     database : 'brain'
   }
 });
@@ -45,37 +47,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // loginEmail[0] --> this used to return the email
-            // TO
-            // loginEmail[0].email --> this now returns the email
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (rq, res) => { register.handleRegister(req, res, db, bcrypt )})
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
