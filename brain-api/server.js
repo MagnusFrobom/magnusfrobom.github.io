a908014acca3a736fdba86af8a33bbca77325b76
Magnus Fröbom <frobom.magnus@gmail.com>
2023-01-25 16:18:47 +0100

    update

--- brain-api/server.js
+++ brain-api/server.js
@@ -4,7 +4,10 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./controllers/register')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
   // Enter your own database information here based on what you created
@@ -28,51 +31,12 @@ app.use(express.json()); // latest version of exressJS now comes with Body-Parse
 //   res.send(database.users);
 // })
 
-app.post('/signin', (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
-
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('./controllers/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 app.post('./controllers/register.js', (req, res) => { register.handleRegister(req, res, db, bcrypt )})
+app.get('/profile/:id', (req, res) => {profile.handleProfileGET(req, res, db, bcrypt)})
+app.put('/image', (req, res) => { image.handlImage(req ,res)})
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
-
-app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-})
-
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
