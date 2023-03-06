806afe48c1f3ec0a9b33b0820ecb3668229f0d08
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-06 18:52:00 +0100

    Revert placeholder pics cause of error

--- react-portf/pages/index.js
+++ react-portf/pages/index.js
@@ -10,11 +10,11 @@ import deved from "../public/avatar.png";
 import code from "../public/hr-python-badge.png";
 import Image from "next/image";
 import web1 from "../public/web1.png";
-import web2 from "../public/web1.png";
-import web3 from "../public/web1.png";
-import web4 from "../public/web1.png";
-import web5 from "../public/web1.png";
-import web6 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 
 export default function Home() {
