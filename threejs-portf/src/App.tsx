2c118c8ffbb497fd4f2e662dccbdddd3f4f8d631
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-12 14:31:53 +0100

    Sections added

--- threejs-portf/src/App.tsx
+++ threejs-portf/src/App.tsx
@@ -1,11 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
-      <div>
+    <BrowserRouter>
+      <div className="realative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
         Magnus Fröbom ThreeJS
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <Contact />
+        <StarsCanvas />
     </div>
+    </BrowserRouter>
   )
 }
 
