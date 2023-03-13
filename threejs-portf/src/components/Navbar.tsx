3b77622131ddf02ef6a21383d42b85f15cf08731
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-13 05:01:13 +0100

    Change index ref to ts.custom navbar, fix tailwind

--- threejs-portf/src/components/Navbar.tsx
+++ threejs-portf/src/components/Navbar.tsx
@@ -5,8 +5,27 @@ import { navLinks } from '../constants/index';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link 
+          to="/"
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive('')
+            window.scrollTo(0, 0);
+          }}
+          >
+            <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+            <p>Magnus Fröbom<span> | SoftwareEngineer | React, Typescript and Python </span>
+
+            </p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
