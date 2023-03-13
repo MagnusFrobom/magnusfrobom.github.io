d9121802d9e8d42691fff200e7c0335977515441
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-13 05:40:39 +0100

    Fixed Smalldevide menu slider

--- threejs-portf/src/components/Navbar.tsx
+++ threejs-portf/src/components/Navbar.tsx
@@ -5,7 +5,8 @@ import { navLinks } from '../constants/index';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState('')
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -20,7 +21,7 @@ const Navbar = () => {
           }}
           >
             <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-            <p>Magnus Fröbom<span> | SoftwareEngineer | React, Typescript and Python </span>
+            <p>Magnus Fröbom<span> | SoftwareEngineer</span>
 
             </p>
         </Link>
@@ -32,7 +33,7 @@ const Navbar = () => {
                 active === Link.title
                 ? "text-white"
                 : "text-secondary"
-              } hover:text-white text-[22px] font-medium cursor-pointer`}
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(Link.title
                 )}
             >
@@ -40,6 +41,16 @@ const Navbar = () => {
             </li>
           ))}
           </ul>
+
+          <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img 
+            src={toggle ? close: menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+            />
+
+          </div>
       </div>
     </nav>
   )
