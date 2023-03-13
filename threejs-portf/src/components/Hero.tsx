7ee6bef66b62c0798c6f577128cc5546b172586b
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-13 06:47:22 +0100

    Started hero section

--- threejs-portf/src/components/Hero.tsx
+++ threejs-portf/src/components/Hero.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='realative w-full mx-auto'>
+      <div>
+        
+      </div>
+    </section>
   )
 }
 
