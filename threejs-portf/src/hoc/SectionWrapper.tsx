a312866e6e1f3ba700dc3f8f646ebd469fc65e07
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 14:34:58 +0100

    Sextion Wrapper complete

--- threejs-portf/src/hoc/SectionWrapper.tsx
+++ threejs-portf/src/hoc/SectionWrapper.tsx
@@ -10,8 +10,11 @@ const SectionWrapper = (Component, idName) => function HOC() {
             initial="hidden"
             whileInView="show"
             viewport={{ once: true, amount: 0.25 }}
-            className=""
+            className={`${styles.padding}, max-w-7xl mx-auto relative z-0`}
         >
+            <span className="hash-span" id={idName}>
+                &nbsp;
+            </span>
         <Component />
         </motion.section>
     )
