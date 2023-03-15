c6ca305854124dcbaff6d09c7a4e420c20f60c44
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 22:08:36 +0100

    Adde Tech balls layout.

--- threejs-portf/src/components/Tech.tsx
+++ threejs-portf/src/components/Tech.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technoLogy) => (
+        <div className="w-28 h-28" key={technoLogy.name}>
+          <BallCanvas icon={technoLogy.icon} />
+        </div>
+    ))}
+    </div>
   )
 }
 
