ca60adc5c6acb496dcd69bcd9922ab097f0247e7
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 14:14:11 +0100

    Adding a Higher order Component SectionWrapper

--- threejs-portf/src/components/About.tsx
+++ threejs-portf/src/components/About.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../style';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard = ({index, title, icon}) => {
   return (
@@ -54,4 +55,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
