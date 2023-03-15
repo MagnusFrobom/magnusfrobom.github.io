43e5409d36fd7e2db9c12e505711b084ba4224c3
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 15:46:45 +0100

    WhiteSPaceBugg?

--- threejs-portf/src/components/Experience.tsx
+++ threejs-portf/src/components/Experience.tsx
@@ -10,8 +10,7 @@ import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
-    contetStyle={{ background: '#1d1836', 
-                   color: '#fff'}}
+    contetStyle={{ background: '#1d1836', color: '#fff'}}
     contentArrowStyle={{ borderRight: '7px solid #232631'}}
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
