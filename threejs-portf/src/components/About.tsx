f739f8e9baf62efd3bc583f7c120a66b34166e7f
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 09:21:34 +0100

    Fix bug in computers and added h2 in about

--- threejs-portf/src/components/About.tsx
+++ threejs-portf/src/components/About.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Tilt from 'react-tilt'
 import { motion } from 'framer-motion';
 
-import { styles } from '../styles';
+import { styles } from '../style';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div>
+        <p></p>
+        <h2>Overview</h2>
+
+      </motion.div>
+    </>
   )
 }
 
--- threejs-portf/src/components/canvas/Computers.tsx
+++ threejs-portf/src/components/canvas/Computers.tsx
@@ -61,7 +61,7 @@ const ComputersCanvas = () => {
             maxPolarAngle={Math.PI /2}
             minPolarAngle={Math.PI /2}
           />
-            <Computers isMobile={isMobile/>
+            <Computers isMobile={isMobile}/>
         </Suspense>
         <Preload all />
     </Canvas>
