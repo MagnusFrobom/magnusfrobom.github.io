764ff4fccc5c42c6a4d6b3261cb34a13d57ab2ae
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-15 21:12:57 +0100

    Added h3

--- threejs-portf/src/components/Experience.tsx
+++ threejs-portf/src/components/Experience.tsx
@@ -10,23 +10,25 @@ import { textVariant } from '../utils/motion';
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
-    contetStyle={{ background: '#1d1836', color: "#fff"}}
-    contentArrowStyle={{ borderRight: '10px solid #532631'}}
+    contentStyle={{ background: '#1d1836', color: "#fff"}}
+    contentArrowStyle={{ borderRight: '7px solid #232631'}}
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
       <div className="flex justify-center items-center w-full h-full">
-        <img
+        <img className="w-[60%] h-[60%]
+          object-contain"
           src={experience.icon}
           alt={experience.company_name}
-          className="w-[60%] h-[60%]
-          object-contain"
+
           />
       </div>
     }
   >
     <div>
-      <h3 className="text-black text-[24px]">{experience.title}</h3>
+      <h3 className="text-white text-[24px]">{experience.title}</h3>
+      <p className="text-secondary text-[16px] font-semibold" style={{ margin: 0 }}>
+        {experience.company_name}</p>
     </div>
 
   </VerticalTimelineElement>
