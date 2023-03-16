dde396c6f8b4b599faf70907dc25e0270e7926c8
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-16 23:01:26 +0100

    Adding Feedback Testimonails

--- threejs-portf/src/components/Feedbacks.tsx
+++ threejs-portf/src/components/Feedbacks.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testimonials } from '../constants';
+
+const FeedbackCard = ( {index, testimonal, name, designation, company, image }) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320p] w-full"
+    >
+    <p>"</p>
+    <p>{testimonal}</p> 
+  </motion.div>
+)
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]">Feedbacks
+      <div className={`${styles
+      .padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What other say</p>
+        <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+      </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
