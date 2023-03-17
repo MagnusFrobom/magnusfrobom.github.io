943834f90d179fe917fdd5753be8c6571f894201
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-17 16:11:09 +0100

    works

--- threejs-portf/src/components/canvas/Earth.tsx
+++ threejs-portf/src/components/canvas/Earth.tsx
@@ -2,11 +2,11 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-import  CanvasLoader from '../Loader';
+import CanvasLoader from '../Loader';
 
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gtlf')
+  const earth = useGLTF('./planet/scene.gltf')
 
   return (
     <primitive 
@@ -24,7 +24,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preseveDrawingBuffer: true }}
-      camera={{}}
+      camera={{ 
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+      }}
       >
 
       <Suspense fallback={<CanvasLoader />}>
@@ -41,4 +46,4 @@ const EarthCanvas = () => {
   )
 }
 
-export default EarthCanvas
+export default EarthCanvas;
