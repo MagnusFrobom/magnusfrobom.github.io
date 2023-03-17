841189701d5234f67ebd1af73f777e93e3d13f67
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-17 12:14:16 +0100

    Finnished Feedbacks themeplate, started Contacts

--- threejs-portf/src/components/Contact.tsx
+++ threejs-portf/src/components/Contact.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from '@emailjs/browser';
+
+import { styles } from "../style";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div>
+        
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
