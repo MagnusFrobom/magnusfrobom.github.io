6c96e3c2437b783df67ddad47a89f3a4f0abd8c8
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-17 12:32:05 +0100

    Added forms

--- threejs-portf/src/components/Contact.tsx
+++ threejs-portf/src/components/Contact.tsx
@@ -22,8 +22,60 @@ const Contact = () => {
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
-      <motion.div>
-        
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className={"flex-[0.75] bg-black-100 p-8 rounded-2xl"}>
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label>
+            <span className="text-white font-medium mb-4">Magnus Fröbom</span>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className="bg-tertiary py-4 px-6 placeholder: text-white rounded-lg outline-none borde-none font-medium"
+            />
+          </label>
+
+                    <label>
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className="bg-tertiary py-4 px-6 placeholder: text-white rounded-lg outline-none borde-none font-medium"
+            />
+          </label>
+
+                    <label>
+            <span className="text-white font-medium mb-4">Your message</span>
+            <textarea
+              rows="7"
+              name="message"
+              value={form.message}
+              onChange={handleChange}
+              placeholder="Type your message here."
+              className="bg-tertiary py-4 px-6 placeholder: text-white rounded-lg outline-none borde-none font-medium"
+            />
+          </label>
+
+          <button
+            type="Submit"
+            >
+              
+          </button>
+
+        </form>
       </motion.div>
     </div>
   )
