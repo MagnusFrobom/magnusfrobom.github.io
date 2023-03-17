3319d9ebc3d7ea92a44492ae912483c19a20a986
Magnus Fröbom <frobom.magnus@gmail.com>
2023-03-17 12:14:25 +0100

    Feedbacks finnsihed

--- threejs-portf/src/components/Feedbacks.tsx
+++ threejs-portf/src/components/Feedbacks.tsx
@@ -3,14 +3,36 @@ import { motion } from 'framer-motion';
 import { styles } from '../style';
 import { fadeIn, textVariant } from '../utils/motion';
 import { testimonials } from '../constants';
+import { SectionWrapper } from '../hoc';
 
-const FeedbackCard = ( {index, testimonal, name, designation, company, image }) => (
+const FeedbackCard = ( {index, testimonial, name, designation, company, image }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
     className="bg-black-200 p-10 rounded-3xl xs:w-[320p] w-full"
     >
-    <p>"</p>
-    <p>{testimonal}</p> 
+    <p className="text-white font-black text-[48px]">"</p>
+    <div className="mt-1">
+    <p>{testimonial}</p> 
+    
+    <div className="mt-7 flex justify-between items-center gap-1">
+      <div className="flex-1 flex flex-col">
+        <p className="text-white font-medium- text-[16px]">
+          <span className="blue-text-gradient">@</span>
+        </p>
+      <p className="mt-1 text-secondary text-[12px]">
+        {designation} of {company}
+      </p>
+      </div>
+
+    <img 
+      src={image}
+      alt={`feedback-by-${name}`}
+      className="w-12 h-10 rounded-full object-cover"
+    />
+
+    </div>
+
+    </div>
   </motion.div>
 )
 
@@ -37,4 +59,4 @@ const Feedbacks = () => {
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "");
